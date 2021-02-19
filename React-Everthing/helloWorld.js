03ba5732927807efcb01479b0e1d7b466ada9084
TobinXu <tommybug@163.com>
2021-02-19 21:27:41 +0800

    commit on master '初始化一下仓库'

---
+++ React-Everthing/helloWorld.js
@@ -0,0 +1,17 @@
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    } 
+    return <h1>Hello, Stranger.</h1>
+}
+
+// 通过引号来将属性指定为字符串字面量，也可以使用大括号将属性值中插入一个js表达式
+const element = <img src={user.avatrUrl} />;
+
+const element1 = <div tabIndex="0"></div>;
+
+// react在渲染的时候会进行转义，防止XSS攻击。
+const title = resposne.potentitalMaliciousInput;
+// 直接使用是安全的：
+const element2 = <h1>{title}</h1>;
+
